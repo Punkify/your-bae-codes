cba365b8ea5f25edb7193c93b9826739cfe65b6d
hrishavs <hrishav.sunar@qantas.com.au>
2024-11-04 17:26:39 +1100

    initial commit

---
+++ Track21/Track21App.swift
@@ -0,0 +1,32 @@
+//
+//  Track21App.swift
+//  Track21
+//
+//  Created by J31065 on 4/11/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Track21App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Track21Tests/Track21Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Track21Tests.swift
+//  Track21Tests
+//
+//  Created by J31065 on 4/11/2024.
+//
+
+import Testing
+@testable import Track21
+
+struct Track21Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
