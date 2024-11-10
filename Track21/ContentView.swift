98ac76743277c71fb970119103cc61e7903507e7
hrishavs <hrishav.sunar@qantas.com.au>
2024-11-10 22:31:51 +1100

    add UI logic filter

--- Track21/ContentView.swift
+++ Track21/ContentView.swift
@@ -18,21 +18,48 @@ struct ContentView: View {
                 ForEach(habits) { habit in
                     NavigationLink {
                         VStack {
-                            Text("Day \(habit.daysCount) has been tracked successfully")
-                                .padding(5)
-                            
-                            HStack{ Button(action: {
+                            if habit.daysCount == 0 {
+                                Text("No tracking for \(habit.name)")
+                                    .padding(5)
+                            }
+                            else if habit.daysCount == 21 {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .font(.system(size: 24)) // Adjust size as needed
+                                    .foregroundColor(.green) // Customize color if needed
+                                Text("Congratulations! You have tracked 21 days in a row!")
+                                    .padding(20)
+                                Image(systemName: "hands.clap")
+                                    .font(.system(size: 24)) // Adjust size as needed
+                                    .foregroundColor(.green) // Customize color if needed
+                            }
+                            else {
+                                Text("Day \(habit.daysCount) has been tracked successfully")
+                                    .padding(10)
+                            }
+                         
+                           
+                            HStack {
+                                
+                                Button(action: {
                                 let habit = trackHabit(habit)
                                 modelContext.insert(habit)
                             }) {
                                 Label("Track", systemImage: "trash")
                             }
+                            .padding(10)
+                                Button(action: {
+                                let habit = undoTrack(habit)
+                                modelContext.insert(habit)
+                            }) {
+                                Label("Undo", systemImage: "arrow.uturn.backward")
+                            }
+                                
                             }
                         }
                         
                     } label: {
                         Text("\(habit.name)")
-                        Text("\(habit.createdAt, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("\(habit.createdAt, format: Date.FormatStyle(date: .numeric, time: .shortened))")
                     }
                 }
                 .onDelete(perform: deleteItems)
