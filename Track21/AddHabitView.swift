59928ada7fe2d46343519a4aabf00ff29dc7d47f
Punkify <hrishavsunar@gmail.com>
2024-11-17 22:35:48 +1100

    feat: add close button in addHabitView

--- Track21/AddHabitView.swift
+++ Track21/AddHabitView.swift
@@ -13,6 +13,8 @@ struct AddHabitView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
+      
+    
         VStack {
             HStack {
                 TextField("Add a habit", text: $habit)
@@ -25,29 +27,52 @@ struct AddHabitView: View {
                     )
             }
             .edgesIgnoringSafeArea(.all)
+            
+            .padding()
+            Button(action: {
+                let habitData = Habit(name: habit)
+                modelContext.insert(habitData)
+                dismiss()
+                print("Habit saved!")
+            }) {
+                Text("Save Habit")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.white, Color.blue]),
+                                       startPoint: .leading,
+                                       endPoint: .trailing)
+                    )
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 200)
+            
+            Button(action: {
+                     
+                dismiss()
+                 })
+            {
+                     Image(systemName: "xmark.circle.fill")
+                         .resizable()
+                         .aspectRatio(contentMode: .fit)
+                         .frame(width: 50, height: 50)
+                         .foregroundStyle(
+                             LinearGradient(
+                                 colors: [.blue, .white],
+                                 startPoint: .top,
+                                 endPoint: .bottom
+                             )
+                         )
+                         .padding()
+                         .background(Circle().fill(Color.gray.opacity(0.2)))
+                         .clipShape(Circle())
+                 }
         }
-        .padding()
-        Button(action: {
-           let habitData = Habit(name: habit)
-            modelContext.insert(habitData)
-            dismiss()
-            print("Habit saved!")
-        }) {
-            Text("Save Habit")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(
-                    LinearGradient(gradient: Gradient(colors: [Color.white, Color.blue]),
-                                   startPoint: .leading,
-                                   endPoint: .trailing)
-                )
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-        }
-        .padding(.horizontal)
-    
+         
         
     }
     
